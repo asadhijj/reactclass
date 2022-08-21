37d61d4a36272a37f6b88aad0aebeefa53b4f73b
asadhijj <hijjawi30@gmail.com>
2022-08-22 00:30:49 +0300

    solved the probelm

---
+++ my-lab1-app/src/components/HornedBeast.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class HornedBeast extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>Markhor</h2>
+                <img src="https://modernfarmer.com/wp-content/uploads/2014/08/saiga.jpg" alt="Markhor"></img>
+                <p>The markhor, according to ARKive, lives in the mountains of central Asia, adeptly climbing craggy rocks with the grace of North America’s own mountain goat. It’s extremely endangered, with an estimated 2,500 left in the wild in part due to hunting for its absurdly spectacular corkscrewed horns, which can grow to more than five feet long.</p>
+            </div>
+
+        )
+    }
+}
+export default HornedBeast;
---
+++ my-lab1-app/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HornedBeast from "./HornedBeast";
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <div>
+               <HornedBeast/>
+               <HornedBeast/>
+            </div>
+        )
+    }
+}
+export default Main;
