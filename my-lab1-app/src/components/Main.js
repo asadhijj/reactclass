4b35971070959b28ca20b47ac8339e3277013728
asadhijj <hijjawi30@gmail.com>
2022-08-24 23:41:24 +0300

    Models

--- my-lab1-app/src/components/Main.js
+++ my-lab1-app/src/components/Main.js
@@ -1,22 +1,17 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import content from "./content"
-
-let beastContent = content.map (item =>
-    <HornedBeast
-      key={item._id}
-      title = {item.title}
-      img ={item.image_url}
-      description = {item.description}
-      />
-    )
 
 class Main extends React.Component {
-
-    render() {
-        return (
-            beastContent
-        )
-    }
+render() {
+  return this.props.content.map((item) => (
+    <HornedBeast
+      key={item._id}
+      title={item.title}
+      img={item.image_url}
+      description={item.description}
+      showModal={this.props.cardModal}
+    />
+  ));
+}
 }
 export default Main;
